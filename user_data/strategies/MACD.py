b7cb6db088d56d48fbf2d5e249fb03c6356663ea
DevsMitra <rahulshr2109@gmail.com>
2022-10-07 19:54:34 +0530

    MACD Updated

--- user_data/strategies/MACD.py
+++ user_data/strategies/MACD.py
@@ -20,13 +20,13 @@ class MACD(IStrategy):
 
     # Optimal stoploss designed for the strategy
     stoploss = -0.1
-    use_custom_stoploss = True
+    use_custom_stoploss = False
 
     # Optimal timeframe for the strategy
     timeframe = '1h'
 
     # Cache
-    # cache = {}
+    cache: dict = {}
 
     def custom_stake_amount(self, pair: str, current_time: datetime, current_rate: float,
                             proposed_stake: float, min_stake: Optional[float], max_stake: float,
@@ -38,23 +38,19 @@ class MACD(IStrategy):
 
     def populate_indicators(self, df: DataFrame, metadata: dict) -> DataFrame:
         df['atr'] = ta.ATR(df)
-        df['adx'] = ta.ADX(df)
+        df['rsi'] = ta.RSI(df)
         mad.madrid(df)
         return df
 
     def populate_entry_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
 
-        con = (
-            (df.shift()['val5'] <= 00) | (df.shift()['val10'] <= 0) | (df.shift()['val15'] <= 0) |
-            (df.shift()['val20'] <= 0) | (df.shift()['val25'] <= 0) | (df.shift()['val30'] <= 0) |
-            (df.shift()['val35'] <= 0) | (df.shift()['val40'] <= 0) | (df.shift()['val45'] <= 0) |
-            (df.shift()['val50'] <= 0) | (df.shift()['val55'] <= 0) | (df.shift()['val60'] <= 0) |
-            (df.shift()['val65'] <= 0) | (df.shift()['val70'] <= 0) | (df.shift()['val75'] <= 0) |
-            (df.shift()['val80'] <= 0) | (df.shift()['val85'] <= 0) | (df.shift()['val90'] <= 0)
-        ) | qtpylib.crossed_above(df['adx'], 20)
+        condition = (
+            ((df['ma5'] > df['ma100']) & qtpylib.crossed_above(df['ma5'], df['ma10'])) |
+            (qtpylib.crossed_above(df['ma5'], df['ma100']) & (df['ma5'] > df['ma10']))
+        ) & (df['rsi'] > 50)
 
         enter_long = (
-            con &
+            condition &
             (df['val5'] > 0) & (df['val10'] > 0) & (df['val15'] > 0) &
             (df['val20'] > 0) & (df['val25'] > 0) & (df['val30'] > 0) &
             (df['val35'] > 0) & (df['val40'] > 0) & (df['val45'] > 0) &
@@ -63,24 +59,14 @@ class MACD(IStrategy):
             (df['val80'] > 0) & (df['val85'] > 0) & (df['val90'] > 0)
         )
 
-        # self.cache[metadata['pair']] = df['low'].rolling(5).min()
         df.loc[enter_long, 'enter_long'] = 1
 
         return df
 
     def populate_exit_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
         df.loc[
-            (
-                qtpylib.crossed_below(df['adx'], 25) |
-                (
-                    (df['val5'] <= 0) & (df['val10'] <= 0) & (df['val15'] <= 0) &
-                    (df['val20'] <= 0) & (df['val25'] <= 0) & (df['val30'] <= 0) &
-                    (df['val35'] <= 0) & (df['val40'] <= 0) & (df['val45'] <= 0) &
-                    (df['val50'] <= 0) & (df['val55'] <= 0) & (df['val60'] <= 0) &
-                    (df['val65'] <= 0) & (df['val70'] <= 0) & (df['val75'] <= 0) &
-                    (df['val80'] <= 0) & (df['val85'] <= 0) & (df['val90'] <= 0)
-                )
-            ),
+            False &
+            qtpylib.crossed_below(df['ma5'], df['ma10']),
             'exit_long'
         ] = 1
         return df
@@ -88,8 +74,11 @@ class MACD(IStrategy):
     def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float,
                            rate: float, time_in_force: str, exit_reason: str,
                            current_time: datetime, **kwargs) -> bool:
-        if (exit_reason == 'force_exit'):
+        profit = trade.calc_profit_ratio(rate)
+        if (((exit_reason == 'force_exit') | (exit_reason == 'exit_signal')) and (profit < 0)):
             return False
+        if pair in self.cache:
+            del self.cache[pair]
         return True
 
     def custom_stoploss(self, pair: str, trade: Trade, current_time: datetime,
@@ -108,13 +97,20 @@ class MACD(IStrategy):
             return get_stoploss(1.1)
         return get_stoploss(4.1)
 
-    def custom_exit(self, pair: str, trade: 'Trade', current_time: 'datetime',
+    def custom_exit(self, pair: str, trade: Trade, current_time: 'datetime',
                     current_rate: float, current_profit: float, **kwargs):
 
         df, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
         candle = df.iloc[-1].squeeze()
 
-        if current_profit > 0:
-            atr = candle['atr']
-            if ((candle['open'] + (atr * 6.1)) < current_rate):
+        if pair not in self.cache:
+            self.cache[pair] = df['low'].rolling(3).min().iloc[-1]
+
+        if pair in self.cache:
+            diff = (trade.open_rate - self.cache[pair]) * 1.1
+            if (((trade.open_rate + diff * 1.5) < current_rate) & (candle['open'] < candle['ma5'])):
+                del self.cache[pair]
                 return 'Profit Booked'
+            if (trade.open_rate - diff) > current_rate:
+                del self.cache[pair]
+                return 'Stop Loss Hit'
