d9e1177ee57f36242a65a40fd210ff12107c95f0
DevsMitra <rahulshr2109@gmail.com>
2023-04-05 19:15:42 +0530

    fix

--- user_data/strategies/NadarayaWatson.py
+++ user_data/strategies/NadarayaWatson.py
@@ -7,21 +7,15 @@ import talib.abstract as ta
 import indicators as indicators
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from freqtrade.persistence import Trade
-
-import BBB as bbb
-
-
 # --------------------------------
-
 
 class NadarayaWatson(IStrategy):
     INTERFACE_VERSION: int = 3
 
-    minimal_roi = {
-        "0":  1
-    }
+    minimal_roi = { "0":  1 }
 
     # Optimal stoploss designed for the strategy
+    sl = 4.1
     stoploss = -0.1
     use_custom_stoploss = True
 
@@ -39,38 +33,19 @@ class NadarayaWatson(IStrategy):
         return self.wallets.get_total_stake_amount() * .06
 
     def populate_indicators(self, df: DataFrame, metadata: dict) -> DataFrame:
-        # bbb.indicator(df)
-        # data1 = indicators.Nadaraya_Watson_Envelope(df)
-        # df['up'] = data1['up']
-        # df['dn'] = data1['dn']
-
-        data = indicators.Nadaraya_Watson(df)
+        data = indicators.Nadaraya_Watson(df, loop_back= 14)
         df['yhat'] = data['yhat']
-        df['yhat2'] = data['yhat2']
-
-        df['ema'] = ta.EMA(df, timeperiod=200)
-        data = ta.MACD(df)
-        df['macd'] = data['macd']
-        df['macdsignal'] = data['macdsignal']
+        df['ema'] = indicators.smma(df, timeperiod=32)
 
         df['atr'] = ta.ATR(df, timeperiod=14)
         return df
 
     def populate_entry_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
         pair = metadata['pair']
-        # if pair in self.custom_info:
-        #     del self.custom_info[pair]
         df.loc[
-            ( 
+            (
                 (df['close'] > df['ema']) &
-                (df['macd'] < 0) &
-                ((
-                    qtpylib.crossed_above(df['yhat2'], df['yhat']) &
-                   (df['macd'] >= df['macdsignal'])
-                ) | (
-                    (df['yhat2'] >= df['yhat']) &
-                    qtpylib.crossed_above(df['macd'], df['macdsignal'])
-                ))
+                qtpylib.crossed_above(df['yhat'], df['yhat'].shift(1))
             ),
             'enter_long'
         ] = 1
@@ -78,63 +53,57 @@ class NadarayaWatson(IStrategy):
 
     def populate_exit_trend(self, df: DataFrame, metadata: dict) -> DataFrame:
         df.loc[
-            (
-                qtpylib.crossed_below(df['yhat2'], df['yhat']) &
-                False
-            ),
+            (qtpylib.crossed_below(df['yhat'], df['yhat'].shift(1)) & False),
             'exit_long'
         ] = 1
         return df
-
-    def custom_stoploss(self, pair: str, trade: Trade, current_time: datetime,
-                    current_rate: float, current_profit: float, **kwargs) -> float:
-        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-        candle = dataframe.iloc[-1].squeeze()
-
-        if pair not in self.custom_info:
-            self.custom_info[pair] = {
-                'last_candle': candle,
-            }
-
-        candle = self.custom_info[pair]['last_candle']
-
-        def get_stoploss(atr):
-            return stoploss_from_absolute(trade.open_rate - (
-                candle['atr'] * atr), trade.open_rate, is_short=trade.is_short
-            ) * -1
-
-        sl = 2.1
-        if (self.profit_target(pair, trade, current_rate, dataframe)):
-            if ((candle['macd'] < candle['macdsignal']) | (candle['yhat2'] < candle['yhat'])):
-                return get_stoploss(.001)
-
-        return get_stoploss(sl)
 
     def confirm_trade_exit(self, pair: str, trade: Trade, order_type: str, amount: float,
                            rate: float, time_in_force: str, exit_reason: str,
                            current_time: datetime, **kwargs) -> bool:
         profit = trade.calc_profit_ratio(rate)
-        if (((exit_reason == 'force_exit') | (exit_reason == 'exit_signal')) and (profit < 0.005)):
+        if (((exit_reason == 'force_exit')) and (profit < 0.01)):
             return False
-
         if pair in self.custom_info:
             del self.custom_info[pair]
         return True
 
-    def profit_target(self, pair, trade, current_rate, df):
+    def trade_candle(self, pair, trade, current_rate):
+        df, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
         last_candle = df.iloc[-1].squeeze()
         if pair not in self.custom_info:
             self.custom_info[pair] = {
                 'last_candle': last_candle,
             }
         last_candle = self.custom_info[pair]['last_candle']
-        pt = trade.open_rate + (last_candle['atr'] * 3)
-        return (pt < current_rate)
+        return last_candle
 
-    # def custom_exit(self, pair: str, trade: Trade, current_time: 'datetime',
-    #                 current_rate: float, current_profit: float, **kwargs):
-    #     df, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-    #     if (self.profit_target(pair, trade, current_rate, df)):
-    #         last_candle = df.iloc[-1].squeeze()
-    #         if ((last_candle['macd'] < last_candle['macdsignal']) | (last_candle['yhat2'] < last_candle['yhat'])):
-    #             return 'Profit Booked'
+    def custom_exit(self, pair: str, trade: Trade, current_time: 'datetime',
+                    current_rate: float, current_profit: float, **kwargs):
+
+        last_candle = self.trade_candle(pair, trade, current_rate)
+        mul = self.sl
+        pt = trade.open_rate + (last_candle['atr'] * mul * 1.5)
+        if (pt < current_rate):
+            return 'Profit Booked'
+
+        sl = trade.open_rate - (last_candle['atr'] * mul)
+        if (sl > current_rate):
+            return 'Stop Loss Hit'
+
+    def custom_stoploss(self, pair: str, trade: Trade, current_time: datetime,
+                    current_rate: float, current_profit: float, **kwargs) -> float:
+        candle = self.trade_candle(pair, trade, current_rate)
+
+        def get_stoploss(atr):
+            return stoploss_from_absolute(
+                current_rate - (candle['atr'] * atr), 
+                current_rate, 
+                is_short=trade.is_short
+            ) * -1
+
+        pt = trade.open_rate + (candle['atr'] * self.sl)
+        if (pt < current_rate):
+            return get_stoploss(self.sl/2)
+
+        return 1
--- user_data/strategies/indicators.py
+++ user_data/strategies/indicators.py
@@ -224,71 +224,75 @@ def poki(df):
 
 # ---------------
 
-# def Nadaraya_Watson(df):
-#     length = 500
-#     h = 8
-#     mult = 3
-#     src =  df['close']
+def Nadaraya_Watson_Envelope(df):
+    length = 100
+    h = 8
+    mult = 3
+    src =  df['close']
 
-#     n = np.arange(len(src))
-#     k = 2
-#     upper = []
-#     lower = []
+    n = np.arange(len(src))
+    k = 2
+    upper = []
+    lower = []
 
-#     for i in range(length//k):
-#         upper.append(np.nan)
-#         lower.append(np.nan)
+    for i in range(length//(k-1)):
+        upper.append(np.nan)
+        lower.append(np.nan)
 
-#     up = np.full(len(src), np.nan)
-#     dn = np.full(len(src), np.nan)
+    up = np.full(len(src), np.nan)
+    dn = np.full(len(src), np.nan)
 
-#     cross_up = 0
-#     cross_dn = 0
+    cross_up = 0
+    cross_dn = 0
 
-#     y = []
-#     sum_e = 0
+    y = []
+    sum_e = 0
     
-#     for i in range(length):
-#         sum_w = 0
-#         sum_y = 0
-#         for j in range(length):
-#             w = math.exp(-((i-j)**2)/(h**2*2))
-#             sum_w += w
-#             sum_y += src[j]*w
-#         y2 = sum_y/sum_w
-#         sum_e += abs(src[i] - y2)
-#         y.append(y2)
+    for i in range(length):
+        sum_w = 0
+        sum_y = 0
+        for j in range(length):
+            w = math.exp(-((i-j)**2)/(h**2*2))
+            sum_w += w
+            sum_y += src[j]*w
+        y2 = sum_y/sum_w
+        sum_e += abs(src[i] - y2)
+        y.append(y2)
         
-#     mae = sum_e/length*mult
+    mae = sum_e/length*mult
     
-#     for i in range(1, length):
-#         y2 = y[i]
-#         y1 = y[i-1]
-#         up[i] = upper[i//k]
-#         dn[i] = lower[i//k]
+    for i in range(1, length):
+        y2 = y[i]
+        y1 = y[i-1]
+        up[i] = upper[i//k]
+        dn[i] = lower[i//k]
         
-#         up[i-k+1:i+1] = [y1+mae]*k
-#         dn[i-k+1:i+1] = [y1-mae]*k
+        up[i-k+1:i+1] = [y1+mae]*k
+        dn[i-k+1:i+1] = [y1-mae]*k
         
-#         # if src[i] > y1 + mae and src[i+1] < y1 + mae:
-#         #     # plt.text(n[-i], src[i], '▼', color=dn_col, ha='center', va='center')
-#         # if src[i] < y1 - mae and src[i+1] > y1 - mae:
-#             # plt.text(n[-i], src[i], '▲', color=up_col, ha='center', va='center')
+        # if src[i] > y1 + mae and src[i+1] < y1 + mae:
+        #     # plt.text(n[-i], src[i], '▼', color=dn_col, ha='center', va='center')
+        # if src[i] < y1 - mae and src[i+1] > y1 - mae:
+            # plt.text(n[-i], src[i], '▲', color=up_col, ha='center', va='center')
             
-#     cross_up = y[0] + mae
-#     cross_dn = y[0] - mae
-#     return {
-#         'up': cross_up,
-#         'dn': cross_dn
-#     }
+    cross_up = y[0] + mae
+    cross_dn = y[0] - mae
+    return {
+        'up': cross_up,
+        'dn': cross_dn
+    }
+
+def smma(df, timeperiod = 32):
+    df['ma'] = ta.SMA(df, timeperiod=timeperiod)
+    smma = (df['ma'].shift(1) * (timeperiod - 1) + df['close']) / timeperiod
+    return smma
 
 
-def Nadaraya_Watson(df):
-    # src = df['close']
+def Nadaraya_Watson(df, loop_back = 8):
     src = df['close'].copy()
     src = src.loc[::-1].reset_index(drop=True)
     # Settings
-    h = 8
+    h = loop_back
     r = 8
     x_0 = 25
     lag = 2
@@ -298,6 +302,8 @@ def Nadaraya_Watson(df):
     def kernel_regression(_src, _size, _h):
         # yhat = [nan] * (x_0 + lag)
         yhat = []
+        sum_es = []
+        sum_e = 0
         for i in range(_size - (x_0 + lag)):
             _currentWeight = 0.
             _cumulativeWeight = 0.
@@ -306,67 +312,46 @@ def Nadaraya_Watson(df):
                 w = math.pow(1 + (math.pow(i-j, 2) / ((math.pow(_h, 2) * 2 * r))), -r)
                 _currentWeight += (y * w)
                 _cumulativeWeight += w
-            yhat.append(_currentWeight / _cumulativeWeight)
+            y2 = _currentWeight / _cumulativeWeight
+            sum_e += abs(src[i] - y2)
+            yhat.append(y2)
+            sum_es.append(sum_e)
 
         for i in range((x_0 + lag)):
             yhat.append(nan)
-        return yhat
+            sum_es.append(nan)
+        return yhat, sum_es
 
     # Estimations
-    yhat11 = kernel_regression(src, size, h)
-    yhat22 = kernel_regression(src, size, h-lag)
+    yhat11, sum_es = kernel_regression(src, size, h)
+    # yhat22, _ = kernel_regression(src, size, h-lag)
 
     yhat11.reverse()
-    yhat22.reverse()
+    # yhat22.reverse()
+    # sum_es.reverse()
+
+    # mult = 3
+    # mae1 = sum_e/size*(mult*2)
+    # mae2 = sum_e/size*(mult+1)
+    # print(sum_es)
+    # up =[nan] * size
+    # dn =[nan] * size
+    # for i in range(size-1):
+    #     mae1 = sum_es[i]/size*(mult*4)
+    #     mae2 = sum_es[i]/size*(mult*2)
+    #     y1 = yhat11[i]
+    #     y2 = yhat11[i+1]
+    #     up[i] = y1 + mae1
+    #     dn[i] = y2 - mae2
 
     yhat1 = pd.Series(yhat11)
-    yhat2 = pd.Series(yhat22)
+    # yhat2 = pd.Series(yhat22)
+    # up = pd.Series(up)
+    # dn = pd.Series(dn)
 
-    # Rates of Change
-    wasBearish = yhat1.shift(2) > yhat1.shift(1)
-    wasBullish = yhat1.shift(2) < yhat1.shift(1)
-    isBearish = yhat1.shift(1) > yhat1
-    isBullish = yhat1.shift(1) < yhat1
-
-    isBearishChange = isBearish & wasBullish
-    isBullishChange = isBullish & wasBearish
-
-    # Crossovers
-    isBullishCross = qtpylib.crossed_above(yhat2, yhat1)
-    isBearishCross = qtpylib.crossed_below(yhat2, yhat1)
-    isBullishSmooth = yhat2 > yhat1
-    isBearishSmooth = yhat2 < yhat1
-
-    # print(isBullishCross)
-
-
-    # for i in range(len(yhat1)-1):
-    #     print(i, yhat1.iloc[i])
     return {
         'yhat': yhat1,
-        'yhat2': yhat2,
-        'bullish': isBullishCross,
-        'bearish': isBearishCross,
+        # 'yhat2': yhat2,
+        # 'up': up,
+        # 'dn': dn,
     }
-
-    # Colors
-    # c_bullish = input.color('#3AFF17', 'Bullish Color', group='Colors')
-    # c_bearish = input.color('#FD1707', 'Bearish Color', group='Colors')
-    # colorByCross = c_bullish if isBullishSmooth else c_bearish
-    # colorByRate = c_bullish if isBullish else c_bearish
-    # plotColor = colorByCross if smoothColors else colorByRate
-
-    # # Plot
-    # plot(yhat1, "Rational Quadratic Kernel Estimate", color=plotColor, linewidth=2)
-
-
-    # // Alert Variables
-    # bool alertBullish = smoothColors ? isBearishCross : isBearishChange
-    # bool alertBearish = smoothColors ? isBullishCross : isBullishChange
-
-    # // Alerts for Color Changes
-    # alertcondition(condition=alertBullish, title='Bearish Color Change', message='Nadaraya-Watson: {{ticker}} ({{interval}}) turned Bearish ▼')
-    # alertcondition(condition=alertBearish, title='Bullish Color Change', message='Nadaraya-Watson: {{ticker}} ({{interval}}) turned Bullish ▲')
-
-    # // Non-Displayed Plot Outputs (i.e., for use in other indicators)
-    # plot(alertBearish ? -1 : alertBullish ? 1 : 0, "Alert Stream", display=display.none)
